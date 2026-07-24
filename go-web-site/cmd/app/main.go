eb94849da00ae5037e6a665b0506e724698cfda6
Alina Babenko <alinababenko.work@gmail.com>
2026-07-24 11:28:19 +0300

    Add test web site fot testing multi-stage building and gitHub packages

---
+++ go-web-site/cmd/app/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// Azure App Service по умолчанию пробрасывает переменную PORT
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "🚀 Hello from Go Web App running on Azure via GHCR!")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Printf("Server starting on port %s...", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
---
+++ go-web-site/go.mod
@@ -0,0 +1,3 @@
+module github.com/your-username/go-ghcr-azure-demo
+
+go 1.22
